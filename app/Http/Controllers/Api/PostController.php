aa88761610f34179fc5541793ea9cfcb185858dd
italomoralesf <profesoritalomoralesf@gmail.com>
2020-02-21 21:49:52 -0500

    API con TDD: Refactorización y validación en Store

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -4,10 +4,17 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\Post as PostRequests;
 
 class PostController extends Controller
 {
+
+    protected $post;
+
+    public function __construct(Post $post)
+    {
+        $this->post = $post;
+    }
 
     /**
      * Display a listing of the resource.
@@ -25,9 +32,9 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostRequests $request)
     {
-        $post = Post::create($request->all());
+        $post = $this->post->create($request->all());
 
         return response()->json($post, 201);
     }
--- tests/Feature/Http/Controllers/Api/PostControllerTest.php
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -14,7 +14,7 @@ class PostControllerTest extends TestCase
 
     public function test_store()
     {
-        $this->withoutExceptionHandling();
+        //$this->withoutExceptionHandling();
         $user = factory(User::class)->create();
 
         $response = $this->actingAs($user, 'api')->json('POST', '/api/posts', [
@@ -26,5 +26,18 @@ class PostControllerTest extends TestCase
             ->assertStatus(201); //OK, creado un recurso
 
         $this->assertDatabaseHas('posts', ['title' => 'El post de prueba']);
+    }
+
+    public function test_validate_title()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user, 'api')->json('POST', '/api/posts', [
+            'title' => ''
+        ]);
+        
+        //Estatus HTTP 422
+        $response->assertStatus(422) 
+            ->assertJsonValidationErrors('title');
     }
 }
